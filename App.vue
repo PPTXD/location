aa37aafa9f309e4bc49a48e1d2440f3a9fb38e10
PPTXD <64525940+PPTXD@users.noreply.github.com>
2020-07-09 14:50:09 +0800

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,30 @@
+<script>
+	
+	export default {
+		globalData:{
+			shareLocation:true,
+			PostCompelete:true,
+			location:{
+				id:'',
+				longitude:'',
+				latitude:'',
+				iconPath:'',
+				width:30,
+				height:30,
+			}
+		},
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
